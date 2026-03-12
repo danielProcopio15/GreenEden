f6bbdeedf76433017d3dd04545caf8ca285894f2
Daniel <danielprocopio15@gmail.com>
2026-03-12 13:12:44 -0300

    first commit/ spring project

---
+++ calculadora-sustentavel/src/main/java/com/greeneden/calculadora_sustentavel/CalculadoraSustentavelApplication.java
@@ -0,0 +1,13 @@
+package com.greeneden.calculadora_sustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraSustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSustentavelApplication.class, args);
+	}
+
+}
---
+++ calculadora-sustentavel/src/test/java/com/greeneden/calculadora_sustentavel/CalculadoraSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeneden.calculadora_sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
